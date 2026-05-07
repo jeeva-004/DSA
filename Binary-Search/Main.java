c7a7416fd7635c5ce2a90b220d68fc7410c0ef3e
jeeva-004 <murugaiyanjeevanantham@gmail.com>
2026-05-07 08:33:58 +0100

    binary search implemented myself:

--- Binary-Search/Main.java
+++ Binary-Search/Main.java
@@ -1,5 +1,7 @@
 class Main {
 
+    // Time Complexity O (Log n)
+
     public static int search(int[] arr, int target) {
 
         int start = 0, end = arr.length;
@@ -16,14 +18,14 @@ class Main {
                 else if (arr[mid] > target)
                     end = mid - 1;
                 else
-                    return mid;
+                    return mid; // {1,2,3,4,5} 2
             } else {
-                if (arr[mid] > target)
-                    end = mid + 1;
-                else if (arr[mid] < target)
-                    end = end - 1;
+                if (arr[mid] < target)
+                    end = mid - 1;
+                else if (arr[mid] > target)
+                    start = mid + 1;
                 else
-                    return mid;
+                    return mid; // {5,4,3,2,1}
             }
         }
 
@@ -31,7 +33,7 @@ class Main {
     }
 
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5 };
-        System.out.println(search(arr, 4));
+        int[] arr = { 5, 4, 3, 2, 1 };
+        System.out.println(search(arr, 5));
     }
 }
