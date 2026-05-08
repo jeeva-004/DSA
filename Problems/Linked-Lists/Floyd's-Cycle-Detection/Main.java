1cabfdf03b29499390376c8f3679f8f2f2d84b8b
jeeva-004 <murugaiyanjeevanantham@gmail.com>
2026-05-08 20:07:50 +0100

    floyd's cycle detection algorithm added

--- Problems/Linked-Lists/Floyd's-Cycle-Detection/Main.java
+++ Problems/Linked-Lists/Floyd's-Cycle-Detection/Main.java
@@ -1,10 +1,69 @@
 import java.util.*;
 
-class Main {
-    static boolean detectCycle(LinkedList<Integer> head){
-        while(){
+class ListNode {
+    int val;
+    ListNode next;
 
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Main {
+
+    static boolean detectCycle(ListNode head) {
+        ListNode rabit = head;
+        ListNode totoys = head;
+        while (rabit != null && rabit.next != null) {
+            rabit = rabit.next.next;
+            totoys = totoys.next;
+            if (rabit == totoys)
+                return true;
         }
         return false;
+    }
+
+    static ListNode returnCycleStart(ListNode head) {
+        ListNode rabit = head;
+        ListNode totoys = head;
+
+        while (rabit != null && rabit.next != null) {
+            rabit = rabit.next.next;
+            totoys = totoys.next;
+            if (rabit == totoys) {
+                totoys = head;
+                while (totoys != rabit) {
+                    totoys = totoys.next;
+                    rabit = rabit.next;
+                }
+                return totoys;
+            }
+        }
+
+        return null;
+    }
+
+    static void removeCycle(ListNode head) {
+        ListNode rabit = head;
+        ListNode totoys = head;
+        while (rabit != null && rabit.next != null) {
+            rabit = rabit.next.next;
+            totoys = totoys.next;
+            ListNode previous = null;
+            if (rabit == totoys) {
+                totoys = head;
+                while (totoys != rabit) {
+                    previous = rabit;
+                    rabit = rabit.next;
+                    totoys = totoys.next;
+                }
+                previous.next = null;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
     }
 }
