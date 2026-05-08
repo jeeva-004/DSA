aa31d97c6d61ef3eb4a5d56dee27072fe6e4b7fc
jeeva-004 <murugaiyanjeevanantham@gmail.com>
2026-05-08 09:36:28 +0100

    bubble sort added

---
+++ Algorithms/Sorting-Algorithms/Bouble-Sort/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Main {
+    // Time Complexity O(n2)
+    static void sortAccending(int[] arr) {
+        boolean swap = false;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swap = true;
+                }
+            }
+            if (!swap)
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 14, 2, 3, 4 };
+        sortAccending(arr);
+        System.out.print(Arrays.toString(arr));
+    }
+}
---
+++ Problems/Linked-Lists/Floyd's-Cycle-Detection/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Main {
+    static boolean detectCycle(LinkedList<Integer> head){
+        while(){
+
+        }
+        return false;
+    }
+}
