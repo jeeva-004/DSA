411a084b97cd919da4fa08b9cf8c7deb250bf4c6
jeeva-004 <murugaiyanjeevanantham@gmail.com>
2026-06-03 08:32:46 +0100

    binary search problem added

---
+++ Problems/Patterns/Binary-Search/Find-Smallest-Letter-Greater-Than-Target/Main.java
@@ -0,0 +1,24 @@
+class Main {
+
+    static char nextGreatestLetter(char[] letters, char target) {
+        if (target == letters[letters.length - 1] || target > letters[letters.length - 1] || target == 'z')
+            return letters[0];
+        int s = 0, e = letters.length - 1;
+        char greatestCharacter = ' ';
+
+        while (s <= e) {
+            int mid = (s + e) / 2;
+            if (target < letters[mid]) {
+                e = mid - 1;
+                greatestCharacter = letters[mid];
+            } else
+                s = mid + 1;
+        }
+        return greatestCharacter;
+    }
+
+    public static void main(String[] args) {
+        char[] letters = { 'c', 'f', 'j' };
+        System.out.println(nextGreatestLetter(letters, 'g'));
+    }
+}
---
+++ Problems/Patterns/Binary-Search/Find-Smallest-Letter-Greater-Than-Target/tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+else if (target < letters[mid]) {
+        //     mid = e - 1;
+        //     while (mid >= 0)
+        //         greatestCharacter = letters[mid];
+        //     mid--;
+        
