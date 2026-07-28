dd3e60d35db13c4f2de3258dbbedc94c278777b3
jeeva-004 <murugaiyanjeevanantham@gmail.com>
2026-07-28 05:03:45 +0100

    Binary search problem added

---
+++ Problems/Patterns/Binary-Search-On-Answer/Capacity-To-Ship-Packages-Within-D-Days/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+
+    static int minCapacityToShipWeights(int[] weights, int days) {
+
+        int min = Integer.MAX_VALUE, max = 0, minCapacity = Integer.MAX_VALUE;
+        int mid = 0;
+        for (int n : weights) {
+            min = Math.min(min, n);
+            max += n;
+        }
+
+        while (min <= max) {
+            mid = (min + max) / 2;
+
+            if (canShip(weights, mid, days)) {
+                minCapacity = Math.min(minCapacity, mid);
+                max = mid - 1;
+            } else
+                min = mid + 1;
+
+        }
+
+        return minCapacity;
+    }
+
+    static boolean canShip(int[] weights, int mid, int days) {
+        int calculatedDays = 1;
+        int weight = 0;
+
+        for (int n : weights) {
+
+            if (weight + n > mid) {
+                calculatedDays++;
+                weight = n;
+            } else
+                weight += n;
+        }
+
+        return calculatedDays <= days;
+    }
+
+    public static void main(String[] args) {
+        int[] weights = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int days = 5;
+
+        System.out.println(minCapacityToShipWeights(weights, days));
+    }
+}
